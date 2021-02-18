6d43175514958972ed60e9dd0f0614b6862b71c0
cavadasA <antonio.cavadas.gu@gmail.com>
2021-02-18 15:27:55 +0000

    Initial commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { FaTimes } from "react-icons/fa";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export function InputTask() {
+	const [tasks, setTask] = useState([]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="row">
+			<div className="offset-md-5">
+				<div className="form-group mb-0">
+					<form>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="What needs to be done?"
+							onKeyPress={() => enterHandler(event)}></input>
+					</form>
+				</div>
+				<ul className="list-group mt-0">{listMaker(tasks)}</ul>
+				<ul className="list-group">
+					<li className="list-group-item">
+						{tasks.length} items left
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
+
+	function listMaker(tasks) {
+		if (tasks.length === 0) {
+			return <li className="list-group-item">No tasks, add a task</li>;
+		}
+		let list = tasks.map(task => {
+			return (
+				<li className="list-group-item" key={task.toString()}>
+					{task}
+					<a href="#" onClick={() => handleRemove(task)}>
+						<FaTimes />
+					</a>
+				</li>
+			);
+		});
+		return list;
+	}
+
+	function enterHandler(e) {
+		if (e.charCode === 13) {
+			e.preventDefault();
+			return setTask(state => [...state, e.target.value]);
+		}
+	}
+
+	function handleRemove(id) {
+		let newList = tasks.filter(task => {
+			return task != id;
+		});
+		return setTask(newList);
+	}
 }
