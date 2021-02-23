b7dc017235f229230d0c8f6689c727bb5ebc6c98
cavadasA <antonio.cavadas.gu@gmail.com>
2021-02-23 11:28:07 +0000

    deleted test file

--- src/js/component/fetcher.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react/cjs/react.production.min";
-
-export function Fetcher() {
-	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/todos")
-			.then(res => res.json())
-			.then(answer => console.log(answer));
-	});
-}
